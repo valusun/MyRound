49d85b12c26810931f27ef093eb2bbe6a99880f4
valusun <chobicoco0327@gmail.com>
2023-02-06 20:30:27 +0900

    add

---
+++ my_round.py
@@ -0,0 +1,27 @@
+def MyRound(val: float, digit: int) -> float:
+    """四捨五入を行う
+
+    Args:
+        val (float): 値
+        digit (int): 桁位置(0を含む正の整数)
+
+    Returns:
+        float: 四捨五入後の値
+
+    Example:
+        MyRound(0.5, 0) -> 1.0\n
+        MyRound(0.55, 1) -> 0.6
+
+    Raises:
+        digitに負の値を渡すとTypeErrorが送出される
+    """
+
+    if digit < 0:
+        raise TypeError
+    bias = 10 ** (digit + 1)
+    ret = val * bias
+    if (amount := ret % 10) >= 5:
+        ret += 10 - amount
+    else:
+        ret -= amount
+    return ret / bias
---
+++ tests/test_my_round.py
@@ -0,0 +1,22 @@
+import pytest
+from .. import my_round
+
+
+class Test_MyRound:
+    @pytest.mark.parametrize(
+        "val, dig, res",
+        [
+            (0.5, 0, 1.0),
+            (0.4, 0, 0.0),
+            (0.6, 0, 1.0),
+            (0.55, 1, 0.6),
+            (0.54, 1, 0.5),
+            (0, 0, 0),
+        ],
+    )
+    def test_Always(self, val, dig, res):
+        assert my_round.MyRound(val, dig) == res
+
+    def test_RaiseTypeError_TakesDigitIsNegative(self):
+        with pytest.raises(TypeError):
+            my_round.MyRound(0.5, -1)
