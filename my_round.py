a5afdfe017e0f9146142aec50d94c4d7a0fedd60
valusun <chobicoco0327@gmail.com>
2023-02-07 08:28:30 +0900

    update/誤差があると気持ち悪いため

--- my_round.py
+++ my_round.py
@@ -1,8 +1,11 @@
-def MyRound(val: float, digit: int) -> float:
+from decimal import Decimal, getcontext
+
+
+def MyRound(val: Decimal, digit: int) -> float:
     """四捨五入を行う
 
     Args:
-        val (float): 値
+        val (Decimal): 値
         digit (int): 桁位置(0を含む正の整数)
 
     Returns:
@@ -25,3 +28,14 @@ def MyRound(val: float, digit: int) -> float:
     else:
         ret -= amount
     return ret / bias
+
+
+def main():
+    getcontext().prec = 3  # 有効桁数の指定
+    # print(0.81 + 0.04)  # 0.8500000000000001
+    print(Decimal(0.81) + Decimal(0.04))  # 0.850
+    print(MyRound(Decimal(0.81) + Decimal(0.04), 1))  # 0.9
+
+
+if __name__ == "__main__":
+    main()
